204ea1678ff18742a862a05b99b3cd61c0851467
Kevin Bandison <kbandison@gmail.com>
2022-12-29 09:28:12 -0500

    added monster list and search box

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
+import CardList from "./components/card-list/card-list.component";
 
-class App extends React.Component() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  onSearch = (event) => {
+    let searchField = event.target.value.toLowerCase();
+
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
+  async componentDidMount() {
+    let response = await fetch("https://jsonplaceholder.typicode.com/users");
+    let users = await response.json();
+    this.setState(() => {
+      return { monsters: users };
+    });
+  }
+
   render() {
+    const { monsters, searchField } = this.state;
+    const { onSearch } = this;
+
+    let filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+    });
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search for Monster"
+          onChange={onSearch}
+        />
+        {filteredMonsters.map((monster) => {
+          return <h1 key={monster.id}>{monster.name}</h1>;
+        })}
+
+        <CardList />
       </div>
     );
   }
